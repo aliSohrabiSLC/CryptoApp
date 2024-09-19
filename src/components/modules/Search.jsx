39e49d0d127822c78e6da2f8db1e7d536bad8b9c
Ali Sohrabi <alisohrabislc@gmail.com>
2024-09-20 01:49:46 +0330

    added a search bar and cleanup and show searched data

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { searchCoin } from "../../services/cryptoApi";
 
 function Search({ currency, setCurrency }) {
+  const [text, setText] = useState("");
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setCoins([]);
+    if (!text) return;
+    const search = async () => {
+      try {
+        const res = await fetch(searchCoin(text), {
+          signal: controller.signal,
+        });
+        const json = await res.json();
+        console.log(json);
+        if (json.coins) {
+          setCoins(json.coins);
+        } else {
+          alert(json.status.error_message);
+        }
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          alert(error.message);
+        }
+      }
+    };
+    search();
+
+    return () => controller.abort();
+  }, [text]);
+
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        placeholder="Search"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
+      <div>
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              <img src={coin.thumb} alt={coin.name} />
+              <p>{coin.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/modules/Tablecoin.jsx
+++ src/components/modules/Tablecoin.jsx
@@ -3,7 +3,7 @@ import { RotatingLines } from "react-loader-spinner";
 import chartUp from "../../assets/chart-up.svg";
 import chartDown from "../../assets/chart-down.svg";
 
-import styles from "./TableCoin.module.css"
+import styles from "./TableCoin.module.css";
 
 function TableCoin({ coins, isLoading }) {
   return (
@@ -55,7 +55,9 @@ const TableRow = ({
       </td>
       <td>{name}</td>
       <td>${current_price.toLocaleString()}</td>
-      <td className={price_change > 0 ? styles.success : styles.error}>{price_change.toFixed(2)}%</td>
+      <td className={price_change > 0 ? styles.success : styles.error}>
+        {price_change.toFixed(2)}%
+      </td>
       <td>{total_volume.toLocaleString()}</td>
       <td>
         <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
