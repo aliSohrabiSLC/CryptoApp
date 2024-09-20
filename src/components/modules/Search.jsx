30a8cd91034367ab95373252e5e863f2a86e6367
Ali Sohrabi <alisohrabislc@gmail.com>
2024-09-20 19:37:14 +0330

    added style for search box called Search.module.css

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { searchCoin } from "../../services/cryptoApi";
+import { RotatingLines } from "react-loader-spinner";
+import styles from "./Search.module.css";
 
 function Search({ currency, setCurrency }) {
   const [text, setText] = useState("");
   const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
     setCoins([]);
-    if (!text) return;
+    if (!text) {
+      setIsLoading(false);
+      return;
+    }
     const search = async () => {
       try {
         const res = await fetch(searchCoin(text), {
@@ -18,6 +24,7 @@ function Search({ currency, setCurrency }) {
         const json = await res.json();
         console.log(json);
         if (json.coins) {
+          setIsLoading(false);
           setCoins(json.coins);
         } else {
           alert(json.status.error_message);
@@ -28,13 +35,14 @@ function Search({ currency, setCurrency }) {
         }
       }
     };
+    setIsLoading(true);
     search();
 
     return () => controller.abort();
   }, [text]);
 
   return (
-    <div>
+    <div className={styles.searchBox}>
       <input
         type="text"
         placeholder="Search"
@@ -46,16 +54,26 @@ function Search({ currency, setCurrency }) {
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
-      <div>
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id}>
-              <img src={coin.thumb} alt={coin.name} />
-              <p>{coin.name}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {(!!coins.length || isLoading)&& (
+        <div className={styles.searchResult}>
+          {isLoading && (
+            <RotatingLines
+              width="50px"
+              height="50px"
+              strokeWidth="2"
+              strokeColor="#3874ff"
+            />
+          )}
+          <ul>
+            {coins.map((coin) => (
+              <li key={coin.id}>
+                <img src={coin.thumb} alt={coin.name} />
+                <p>{coin.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
